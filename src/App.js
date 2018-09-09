2357028cf06bc8a9038f7ccb98d947ebe132e731
Ankur Sharma <ankur_sharma309@yahoo.com>
2018-09-09 12:14:21 +0200

    trying react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,138 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Album from './components/Album';
 import SignIn from "./components/SignIn";
 
+import {createMuiTheme} from '@material-ui/core/styles';
+import indigo from '@material-ui/core/colors/indigo';
+import green from '@material-ui/core/colors/green';
+import MuiThemeProvider from "@material-ui/core/es/styles/MuiThemeProvider";
+import {
+    withRouter,
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Redirect
+} from "react-router-dom";
+
+const theme = createMuiTheme({
+    palette: {
+//        primary: indigo,
+//        type: "dark",
+    },
+});
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Ankurs React App, under active development</h1>
-        </header>
-          <div>
-              <Album/>
-              <SignIn/>
-          </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <MuiThemeProvider theme={theme}>
+                <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">Welcome to Ankurs React App, under active development</h1>
+                        <ul>
+                            <li>
+                                <Link to="/public">Public Page</Link>
+                            </li>
+                            <li>
+                                <Link to="/protected">Protected Page</Link>
+                            </li>
+                        </ul>
+                    </header>
+                    <div>
+                            <div>
+                                <AuthButton />
+                                <Route path="/public" component={Public} />
+                                <Route path="/login" component={Login} />
+                                <PrivateRoute path="/protected" component={Protected} />
+                            </div>
+                    </div>
+                </div>
+                </Router>
+            </MuiThemeProvider>
+        );
+    }
+}
+
+const fakeAuth = {
+    isAuthenticated: false,
+    authenticate(cb) {
+        this.isAuthenticated = true;
+        setTimeout(cb, 100); // fake async
+    },
+    signout(cb) {
+        this.isAuthenticated = false;
+        setTimeout(cb, 100);
+    }
+};
+
+const AuthButton = withRouter(
+    ({ history }) =>
+        fakeAuth.isAuthenticated ? (
+            <p>
+                Welcome!{" "}
+                <button
+                    onClick={() => {
+                        fakeAuth.signout(() => history.push("/"));
+                    }}
+                >
+                    Sign out
+                </button>
+            </p>
+        ) : (
+            <p>You are not logged in.</p>
+        )
+);
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+        {...rest}
+        render={props =>
+            fakeAuth.isAuthenticated ? (
+                <Component {...props} />
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: "/login",
+                        state: { from: props.location }
+                    }}
+                />
+            )
+        }
+    />
+);
+
+const Public = () => <h3>Public</h3>;
+const Protected = () => <h3>Protected</h3>;
+
+class Login extends React.Component {
+    state = {
+        redirectToReferrer: false
+    };
+
+    login = () => {
+        fakeAuth.authenticate(() => {
+            this.setState({ redirectToReferrer: true });
+        });
+    };
+
+    render() {
+        const { from } = this.props.location.state || { from: { pathname: "/" } };
+        const { redirectToReferrer } = this.state;
+
+        if (redirectToReferrer) {
+            return <Redirect to={from} />;
+        }
+
+        return (
+            <div>
+                <p>You must log in to view the page at {from.pathname}</p>
+                <button onClick={this.login}>Log in</button>
+            </div>
+        );
+    }
 }
 
 export default App;
